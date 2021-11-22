2d645cbce957bd901ebb1ca0354a4d7b36d1d430
MishaDak <dack.misha@gmail.com>
2021-11-22 22:48:25 +0700

    Add select action

---
+++ lib/states/select_action.rb
@@ -0,0 +1,13 @@
+require_relative 'base'
+require_relative 'player_state'
+
+module GameStates
+  class ResetPlayer < Base
+    def render; end
+
+    def next
+      context[:valera] = Valera.new
+      SavePlayer.new context
+    end
+  end
+end
