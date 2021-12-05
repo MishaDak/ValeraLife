e6b6cd733f2921de60d42b285c8d56484bfaad70
ChtobiUchitsyaNadoBitProfi <ivan.mielnikov.01@mail.ru>
2021-12-05 19:18:38 +0700

    add save_player

--- lib/states/save_player.rb
+++ lib/states/save_player.rb
@@ -1,1 +1,16 @@
+require_relative "./base"
+require_relative "./select_action"
+require_relative "../configs/config_adapter"
 
+module GameStates
+  class SavePlayer < Base
+    def render
+
+    end
+
+    def next
+      ConfigAdapter.instance.save "configs/saved_states.yml", context[:valera]
+      PlayerState.new context
+    end
+  end
+end
